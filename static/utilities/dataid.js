35c703c5d2f6df1438a1e679fd41016aa598eb6f
caH40 <kmvsancho@gmail.com>
2022-04-22 22:22:38 +0300

    добавил валидацию вводимых данных при авторизации

--- static/utilities/dataid.js
+++ static/utilities/dataid.js
@@ -1,31 +1,42 @@
 import authPage from '../pages/auth.js';
-const body = document.querySelector('body');
-const hamb = document.querySelector('#hamb');
-const popup = document.querySelector('#popup');
-//cloneNode глубокое клонирование
-// const menu = document.querySelector('#menu').cloneNode(1);
-hamb.addEventListener('click', (e) => {
-  e.preventDefault();
-  //toggle при первом нажатии добавляется, при втором убирается
-  popup.classList.toggle('open');
-  renderPopup();
+try {
+  const body = document.querySelector('body');
+  const hamb = document.querySelector('#hamb');
+  const popup = document.querySelector('#popup');
+  const bar = document.querySelectorAll('.bar');
+  //cloneNode глубокое клонирование
+  // const menu = document.querySelector('#menu').cloneNode(1);
+  hamb.addEventListener('click', (e) => {
+    e.preventDefault();
+    //toggle при первом нажатии добавляется, при втором убирается
+    popup.classList.toggle('open');
+    renderPopup();
 
-  if (popup.classList.contains('open')) {
-    body.style.position = 'fixed';
-  } else {
-    body.style.position = 'unset';
+    if (popup.classList.contains('open')) {
+      body.style.position = 'fixed';
+      bar[0].classList.add('bar-x1');
+      bar[1].classList.add('bar-x2');
+      bar[2].classList.add('bar-x3');
+    } else {
+      body.style.position = 'unset';
+      bar[0].classList.remove('bar-x1');
+      bar[1].classList.remove('bar-x2');
+      bar[2].classList.remove('bar-x3');
+    }
+  });
+
+  //попап с авторизацией
+  authPage();
+
+  function renderPopup() {
+    console.log(menu);
+    menu.classList.add('menu__popup');
+    popup.appendChild(menu);
   }
-});
 
-//попап с авторизацией
-authPage();
-
-function renderPopup() {
-  console.log(menu);
-  menu.classList.add('menu__popup');
-  popup.appendChild(menu);
+  const dataId = body.getAttribute('data-id-page');
+  const navItem = document.querySelector(`[data-id-nav="${dataId}"]`);
+  navItem.classList.add('active-nav');
+} catch (error) {
+  console.log(error);
 }
-
-const dataId = body.getAttribute('data-id-page');
-const navItem = document.querySelector(`[data-id-nav="${dataId}"]`);
-navItem.classList.add('active-nav');
