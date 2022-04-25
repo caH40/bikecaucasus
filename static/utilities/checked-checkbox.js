1212b45d38267f0c62614c95b172006c962a92e5
caH40 <kmvsancho@gmail.com>
2022-04-25 11:22:49 +0300

    мелке изменения в filter-trails

--- static/utilities/checked-checkbox.js
+++ static/utilities/checked-checkbox.js
@@ -1,7 +1,9 @@
 export default function () {
   let state = JSON.parse(localStorage.getItem('state'));
   let bikeType = JSON.parse(localStorage.getItem('bikeType'));
+
   const checkboxes = document.querySelectorAll('[name="checkbox-filter"]');
+  //установка галок в чекбоксах для значений из localStorage
   checkboxes.forEach((checkbox) => {
     if (
       state.includes(checkbox.defaultValue) ||
@@ -12,7 +14,6 @@ export default function () {
   });
   //подсвечивание лампочки фильтров
   const flashlight = document.querySelector('.trail__flashlight');
-  console.log(state.length, bikeType.length);
   if (state.length === 4 && bikeType.length === 2) {
     flashlight.classList.add('trail__flashlight-off');
   } else {
--- static/utilities/filter-trails.js
+++ static/utilities/filter-trails.js
@@ -3,7 +3,8 @@ export default function filterTrails(cards) {
 
   let state = JSON.parse(localStorage.getItem('state'));
   let bikeType = JSON.parse(localStorage.getItem('bikeType'));
-  //первоначальные установки отображение, показывать всё, если в localstorage нет данных
+  // ================================================================
+  //первоначальные установки отображение, показывать всё, если в localStorage нет данных
   //если снять все галки, то автоматически проставляются галки??? убирать или нет???
   if (!state || state.length === 0) {
     state = [
@@ -19,25 +20,24 @@ export default function filterTrails(cards) {
     bikeType = ['Шоссейный', 'Горный'];
   }
   localStorage.setItem('bikeType', JSON.stringify(bikeType));
+  // ================================================================
 
-  let filters = {
-    state,
-    bikeType,
-  };
-  // console.log(filters);
-  let lengthState = filters.state.length;
-  let lengthBikeType = filters.bikeType.length;
+  let lengthState = state.length;
+  let lengthBikeType = bikeType.length;
 
-  const countState = 4;
-  const filterState = cards.filter((card) => {
-    if (lengthState === countState) {
+  // const countState = 4;
+  const filteredState = cards.filter((card) => {
+    if (
+      state.includes('КавМинВоды') &&
+      state.includes('Карачаево-Черкессия') &&
+      state.includes('Кабардино-Балкария') &&
+      state.includes('Северная Осетия')
+    ) {
       return cards;
     }
-
     let compare = true;
-
     for (let i = 0; i < lengthState; i++) {
-      if (card.state === filters.state[i]) {
+      if (card.state === state[i]) {
         return (compare = true);
       } else {
         compare = false;
@@ -46,16 +46,14 @@ export default function filterTrails(cards) {
     compare;
   });
 
-  const countBikeType = 2;
-  const filteredCards = filterState.filter((card) => {
-    if (lengthBikeType === countBikeType) {
-      return filterState;
+  // const countBikeType = 2;
+  const filteredCards = filteredState.filter((card) => {
+    if (bikeType.includes('Шоссейный') && bikeType.includes('Горный')) {
+      return filteredState;
     }
-
     let compare = true;
-
     for (let i = 0; i < lengthBikeType; i++) {
-      if (card.bikeType === filters.bikeType[i]) {
+      if (card.bikeType === bikeType[i]) {
         return (compare = true);
       } else {
         compare = false;
