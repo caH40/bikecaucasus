58992fac45937458d877c6b7f60ee4f20496ccff
caH40 <kmvsancho@gmail.com>
2022-05-06 07:29:59 +0300

    где то cors включился...

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import 'dotenv/config';
 import express from 'express';
 import mongoose from 'mongoose';
 import path from 'path';
+import cors from 'cors';
 
 import serverRoutes from './routes/routes.js';
 import authRouter from './routes/authRouter.js';
@@ -14,6 +15,7 @@ app.use(express.json({ limit: '25mb' }));
 // app.use(express.urlencoded({ extended: false }));
 app.use(express.urlencoded({ limit: '25mb' }));
 app.use(express.static(path.resolve(__dirname, 'static')));
+app.use(cors());
 // app.use(multer({ dest: 'uploads' }).single('filedata'));
 // CORS;
 app.use(function (req, res, next) {
