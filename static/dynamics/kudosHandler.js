115e7b1780328cced02b5727bd7bb88f96828d93
caH40 <kmvsancho@gmail.com>
2022-05-09 17:58:22 +0300

    добавил блоки try/catch; заменил иконку логина

--- static/dynamics/kudosHandler.js
+++ static/dynamics/kudosHandler.js
@@ -2,38 +2,50 @@ import addPlus from '../utilities/addPlus.js';
 
 //динамика лайка
 export function handlerLike(dataFromDb) {
-  const kudosNumber = document.querySelector('#block-kudos__number');
-  const kudosFillGreen = document.querySelector('#kudos__green-fill-2');
-  const kudosFillRed = document.querySelector('#kudos__red-fill-2');
-  if (dataFromDb.remove) {
-    kudosFillGreen.classList.remove('classLike');
-  } else {
-    kudosFillGreen.classList.add('classLike');
-    kudosFillRed.classList.remove('classDisLike');
+  try {
+    const kudosNumber = document.querySelector('#block-kudos__number');
+    const kudosFillGreen = document.querySelector('#kudos__green-fill-2');
+    const kudosFillRed = document.querySelector('#kudos__red-fill-2');
+    if (dataFromDb.remove) {
+      kudosFillGreen.classList.remove('classLike');
+    } else {
+      kudosFillGreen.classList.add('classLike');
+      kudosFillRed.classList.remove('classDisLike');
+    }
+    kudosNumber.innerHTML = addPlus(dataFromDb.kudosGoodQuantity);
+  } catch (error) {
+    console.log(error);
   }
-  kudosNumber.innerHTML = addPlus(dataFromDb.kudosGoodQuantity);
 }
 
 //динамика дизлайка
 export function handlerDisLike(dataFromDb) {
-  const kudosNumber = document.querySelector('#block-kudos__number');
-  const kudosFillGreen = document.querySelector('#kudos__green-fill-2');
-  const kudosFillRed = document.querySelector('#kudos__red-fill-2');
-  if (dataFromDb.remove) {
-    kudosFillRed.classList.remove('classDisLike');
-  } else {
-    kudosFillGreen.classList.remove('classLike');
-    kudosFillRed.classList.add('classDisLike');
+  try {
+    const kudosNumber = document.querySelector('#block-kudos__number');
+    const kudosFillGreen = document.querySelector('#kudos__green-fill-2');
+    const kudosFillRed = document.querySelector('#kudos__red-fill-2');
+    if (dataFromDb.remove) {
+      kudosFillRed.classList.remove('classDisLike');
+    } else {
+      kudosFillGreen.classList.remove('classLike');
+      kudosFillRed.classList.add('classDisLike');
+    }
+    kudosNumber.innerHTML = addPlus(dataFromDb.kudosGoodQuantity);
+  } catch (error) {
+    console.log(error);
   }
-  kudosNumber.innerHTML = addPlus(dataFromDb.kudosGoodQuantity);
 }
 //динамика попап сообщения
 export function handlerMessagePopup() {
-  const messagePopup = document.querySelector('#message__popup');
-  messagePopup.classList.remove('message__popup-invisible');
-  messagePopup.classList.add('message__popup-visible');
-  setTimeout(() => {
-    messagePopup.classList.add('message__popup-invisible');
-    messagePopup.classList.remove('message__popup-visible');
-  }, 500);
+  try {
+    const messagePopup = document.querySelector('#message__popup');
+    messagePopup.classList.remove('message__popup-invisible');
+    messagePopup.classList.add('message__popup-visible');
+    setTimeout(() => {
+      messagePopup.classList.add('message__popup-invisible');
+      messagePopup.classList.remove('message__popup-visible');
+    }, 500);
+  } catch (error) {
+    console.log(error);
+  }
 }
--- static/utilities/myfetch.js
+++ static/utilities/myfetch.js
@@ -2,14 +2,18 @@ import { host } from '../utilities/host.js';
 
 export default {
   async fetchPost(url, bodyObj) {
-    const dataFromDb = await fetch(`${host}${url}`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        authorization: localStorage.getItem('tokenBikeCaucasus'),
-      },
-      body: JSON.stringify(bodyObj),
-    }).then((data) => data.json());
-    return dataFromDb;
+    try {
+      const dataFromDb = await fetch(`${host}${url}`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          authorization: localStorage.getItem('tokenBikeCaucasus'),
+        },
+        body: JSON.stringify(bodyObj),
+      }).then((data) => data.json());
+      return dataFromDb;
+    } catch (error) {
+      console.log(error);
+    }
   },
 };
