c05094815763242986c9d68be70e5a8e8f62ee27
caH40 <kmvsancho@gmail.com>
2022-05-09 16:42:36 +0300

    систематизация кода обработки кудосов; вынес в модуль обработку динамикических элементов

--- static/routes/router-description.js
+++ static/routes/router-description.js
@@ -8,37 +8,46 @@ import {
 export default {
   async getKudos(cardId) {
     const blockKudos = document.querySelector('#block-kudos__inner');
-
     //like
     blockKudos.addEventListener('click', async (event) => {
-      const idSelector = event.target.id;
-
       //если нет нужных селекторов то ретурн
-      if (!['kudos-green', 'kudos-red'].includes(idSelector)) {
+      if (
+        !(
+          event.target.closest('#kudos-green') ||
+          event.target.closest('#kudos-red')
+        )
+      ) {
         return;
       }
 
       let dataFromDb;
       //Like
-      if (idSelector === 'kudos-green') {
+      if (event.target.closest('#kudos-green')) {
         dataFromDb = await myFetch.fetchPost('/kudos', {
           cardId,
           kudos: true,
         });
+
+        if (dataFromDb.noAuthorization) {
+          handlerMessagePopup();
+          return;
+        }
+        handlerLike(dataFromDb);
       }
+
       //disLike
-      if (idSelector === 'kudos-red') {
+      if (event.target.closest('#kudos-red')) {
         dataFromDb = await myFetch.fetchPost('/kudos', {
           cardId,
           kudos: false,
         });
-      }
 
-      if (dataFromDb.noAuthorization) {
-        handlerMessagePopup();
-        return;
+        if (dataFromDb.noAuthorization) {
+          handlerMessagePopup();
+          return;
+        }
+        handlerDisLike(dataFromDb);
       }
-      handlerLike(dataFromDb);
     });
   },
 };
