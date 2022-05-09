87ee350748e80b9a552eeb7b647d6e9c789e512c
caH40 <kmvsancho@gmail.com>
2022-05-09 21:18:03 +0300

    небольшие изменения

--- Model/Сomment.js
+++ Model/Сomment.js
@@ -1,11 +1,13 @@
 import prk from 'mongoose';
 const { Schema, model } = prk;
 
-const kudosSchema = new Schema({
+const commentSchema = new Schema({
   cardId: { type: String, unique: true, required: true },
+  text: { type: String },
+  userId: { type: String },
+  date: { type: String },
   usersIdLike: [{ type: String }],
   usersIdDisLike: [{ type: String }],
-  views: { type: Number, default: 0 },
 });
 
-export default model('Kudos', kudosSchema);
+export default model('Comment', commentSchema);
