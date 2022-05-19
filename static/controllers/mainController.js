7d62fea684ece2550c2d5769561e1ffe751dbf4b
caH40 <kmvsancho@gmail.com>
2022-05-19 08:43:07 +0300

    сделана форма добавления новости

--- static/controllers/mainController.js
+++ static/controllers/mainController.js
@@ -20,11 +20,39 @@ export default {
       dataFormDb.news.forEach((news) => {
         news.date = new Date(Number(news.date)).toLocaleDateString();
       });
-      console.log(dataFormDb.news[0]);
+      // console.log(dataFormDb.news[0]);
       const dataTemplate = { list: dataFormDb.news };
       render(dataTemplate, '#mainTemplate');
     } catch (error) {
       console.log(error);
     }
+  },
+  createNews(idNews) {
+    const blockNews = document.querySelector(`#block-news-${idNews}`);
+    blockNews.insertAdjacentHTML(
+      'afterend',
+      `
+      <div class="news-create" id="news-create">
+        <h4 class="news-create__title">Блок публикации новостей</h4>
+        <div class="news-create__block">
+            <div class="news-create__remove-cross" id="news-create__remove-cross">&times</div>
+            <div class="news-create__image" id="news-create__image"></div>
+            <form class="news-create__form">
+                <input class="news-create__input" type="text" placeholder="Заголовок новости"
+                    id="news-create__input">
+                <textarea class="news-create__area" name="create-news" rows="5"
+                    id="news-create__area"></textarea>
+                <button class="news-create__btn" id="news-create__image-btn"
+                    type="submit">Загрузить</button>
+                <input class="profile__img-input" type="file" accept=".jpg, .jpeg, .png, .webp"         id="news-create__image-input">
+                <button class="news-create__btn" type="submit"
+                    id="news-create__btn-send">Опубликовать</button>
+            </form>
+        </div>
+      </div>
+    `
+    );
+    //прослушка окна публикации новости
+    router.createNewsBlock();
   },
 };
--- static/routes/router-main.js
+++ static/routes/router-main.js
@@ -3,5 +3,117 @@ import controller from '../controllers/mainController.js';
 export default {
   async main() {
     await controller.main();
+    this.menuNews();
+  },
+  async iconsNews() {},
+  async menuNews() {
+    const mainInnerNews = document.querySelector('#main__inner-news');
+
+    mainInnerNews.addEventListener('click', (event) => {
+      if (!event.target.matches('#popap-news__arrow-box')) return;
+      const idNews = event.target.dataset.id;
+
+      this.openMenu(idNews);
+    });
+  },
+  openMenu(idNews, index) {
+    const menuNewsCurrent = document.querySelector(`#popap-news-${idNews}`);
+    const popapMenuNewsCurrent = document.querySelector(`#newsId-${idNews}`);
+    const arrowMenuNewsCurrentPath = document.querySelector(`#popap-news__arrow-${idNews}  > path`);
+
+    popapMenuNewsCurrent.classList.add('visible');
+    arrowMenuNewsCurrentPath.classList.add('fill-arrow');
+
+    const newsCreateBtn = document.querySelector(`#create-${idNews}`);
+    const newsEditBtn = document.querySelector(`#edit-${idNews}`);
+    const newsDeleteBtn = document.querySelector(`#remove-${idNews}`);
+
+    newsCreateBtn.addEventListener('click', handlerCreate);
+    function handlerCreate() {
+      popapMenuNewsCurrent.classList.remove('visible');
+      arrowMenuNewsCurrentPath.classList.remove('fill-arrow');
+      controller.createNews(idNews);
+    }
+    newsEditBtn.addEventListener('click', handlerEdit);
+    function handlerEdit() {
+      popapMenuNewsCurrent.classList.remove('visible');
+      arrowMenuNewsCurrentPath.classList.remove('fill-arrow');
+    }
+
+    newsDeleteBtn.addEventListener('click', handlerRemove);
+    function handlerRemove() {
+      popapMenuNewsCurrent.classList.remove('visible');
+      arrowMenuNewsCurrentPath.classList.remove('fill-arrow');
+    }
+
+    menuNewsCurrent.addEventListener('mouseleave', handlerFocusOut, {
+      once: true,
+    });
+    function handlerFocusOut() {
+      popapMenuNewsCurrent.classList.remove('visible');
+      arrowMenuNewsCurrentPath.classList.remove('fill-arrow');
+      newsCreateBtn.removeEventListener('click', handlerCreate);
+      newsEditBtn.removeEventListener('click', handlerEdit);
+      newsDeleteBtn.removeEventListener('click', handlerRemove);
+    }
+  },
+  createNewsBlock() {
+    const blockNewsCreate = document.querySelector('#news-create');
+    const loadImageBlock = document.querySelector('#news-create__image');
+    const loadImageBtn = document.querySelector('#news-create__image-btn');
+    const loadImageInput = document.querySelector('#news-create__image-input');
+    const closeCross = document.querySelector('#news-create__remove-cross');
+    const sendForm = document.querySelector('#news-create__btn-send');
+
+    //закрытие блока публикации новостей
+    closeCross.addEventListener(
+      'click',
+      () => {
+        blockNewsCreate.remove();
+      },
+      { once: true }
+    );
+
+    // кнопка загрузки картинки
+    loadImageBtn.addEventListener('click', handlerLoadBtn);
+    function handlerLoadBtn(event) {
+      event.preventDefault();
+      loadImageInput.click();
+    }
+
+    //интуп выбора файла картинки
+    loadImageInput.addEventListener('change', handlerLoadFile);
+    function handlerLoadFile(event) {
+      const file = event.target.files[0];
+      loadImageBlock.innerHTML = '';
+
+      const reader = new FileReader();
+      reader.onload = async (e) => {
+        const src = e.target.result;
+
+        let sizeFile = Math.trunc(file.size / 8000);
+        loadImageBlock.insertAdjacentHTML(
+          'afterbegin',
+          `<div class="news-create__preview">
+          <div class="news-create__cross" id="news-create__cross">&times</div>
+            <img src="${src}" id="photo-desc-img"/>
+            <div class="news-create__preview-name">
+              <span>${file.name}</span>
+              <span>${sizeFile}kB</span>
+            </div>
+          </div>`
+        );
+
+        const cross = document.querySelector('#news-create__cross');
+        cross.addEventListener(
+          'click',
+          () => {
+            loadImageBlock.innerHTML = '';
+          },
+          { once: true }
+        );
+      };
+      reader.readAsDataURL(file);
+    }
   },
 };
