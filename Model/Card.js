06240dad28583dd529861e5b00b9b8cbaadbf1dd
caH40 <kmvsancho@gmail.com>
2022-06-12 17:00:29 +0300

    исправил баг с датой создания маршрута

--- Model/Card.js
+++ Model/Card.js
@@ -1,26 +1,26 @@
-import mongoose from 'mongoose';
-import pkg from 'mongoose';
-const { Schema, model } = pkg;
+import mongoose from 'mongoose'
+import pkg from 'mongoose'
+const { Schema, model } = pkg
 
 const cardSchema = new Schema({
-  nameRoute: { type: String, required: true },
-  state: { type: String },
-  bikeType: { type: String },
-  start: { type: String },
-  turn: { type: String },
-  finish: { type: String },
-  distance: { type: String },
-  ascent: { type: String },
-  descriptionArea: { type: String },
-  cardPhoto: { type: String },
-  fileTrekName: { type: String },
-  urlVideo: { type: String },
-  urlTrekGConnect: { type: String },
-  postedBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-  comments: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Comment' }],
-  kudoses: { type: mongoose.Schema.Types.ObjectId, ref: 'Kudos' },
-  dateCreate: { type: Number },
-  dateEdit: { type: Number },
-});
+	nameRoute: { type: String, required: true },
+	state: { type: String },
+	bikeType: { type: String },
+	start: { type: String },
+	turn: { type: String },
+	finish: { type: String },
+	distance: { type: String },
+	ascent: { type: String },
+	descriptionArea: { type: String },
+	cardPhoto: { type: String },
+	fileTrekName: { type: String },
+	urlVideo: { type: String },
+	urlTrekGConnect: { type: String },
+	postedBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+	comments: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Comment' }],
+	kudoses: { type: mongoose.Schema.Types.ObjectId, ref: 'Kudos' },
+	date: { type: Number },
+	dateEdit: { type: Number },
+})
 
-export default model('Card', cardSchema);
+export default model('Card', cardSchema)
