0e9ad1e76f87a346b1cdf4747fdc5d386152c42e
caH40 <kmvsancho@gmail.com>
2022-06-12 18:54:19 +0300

    изменил разрешение фотографии на карточке маршрута

--- static/utilities/load-img.js
+++ static/utilities/load-img.js
@@ -1,72 +1,72 @@
-import reduceImage from './reduce-image.js';
+import reduceImage from './reduce-image.js'
 export default function loadimg(data) {
-  const inputAddCardPhoto = document.querySelector('#input__card-photo');
-  const inputAddDescPhoto = document.querySelector('#input__desc-photo');
-  const inputAddTrek = document.querySelector('#input__trek');
+	const inputAddCardPhoto = document.querySelector('#input__card-photo')
+	const inputAddDescPhoto = document.querySelector('#input__desc-photo')
+	const inputAddTrek = document.querySelector('#input__trek')
 
-  const buttonAddCardPhoto = decorateInput(inputAddCardPhoto, 'ВЫБРАТЬ ФАЙЛ');
-  const buttonAddDescPhoto = decorateInput(inputAddDescPhoto, 'ВЫБРАТЬ ФАЙЛЫ');
-  const buttonAddTrek = decorateInput(inputAddTrek, 'ВЫБРАТЬ ФАЙЛ');
+	const buttonAddCardPhoto = decorateInput(inputAddCardPhoto, 'ВЫБРАТЬ ФАЙЛ')
+	const buttonAddDescPhoto = decorateInput(inputAddDescPhoto, 'ВЫБРАТЬ ФАЙЛЫ')
+	const buttonAddTrek = decorateInput(inputAddTrek, 'ВЫБРАТЬ ФАЙЛ')
 
-  function decorateInput(input, text) {
-    const button = document.createElement('div');
-    button.classList.add('box__btn', 'div__btn');
-    button.textContent = text;
-    input.insertAdjacentElement('afterend', button);
-    return button;
-  }
+	function decorateInput(input, text) {
+		const button = document.createElement('div')
+		button.classList.add('box__btn', 'div__btn')
+		button.textContent = text
+		input.insertAdjacentElement('afterend', button)
+		return button
+	}
 
-  //при клике на button тригерится input.click()
-  buttonAddCardPhoto.addEventListener('click', () => {
-    inputAddCardPhoto.click();
-  });
-  buttonAddDescPhoto.addEventListener('click', () => {
-    inputAddDescPhoto.click();
-  });
-  buttonAddTrek.addEventListener('click', () => {
-    inputAddTrek.click();
-  });
+	//при клике на button тригерится input.click()
+	buttonAddCardPhoto.addEventListener('click', () => {
+		inputAddCardPhoto.click()
+	})
+	buttonAddDescPhoto.addEventListener('click', () => {
+		inputAddDescPhoto.click()
+	})
+	buttonAddTrek.addEventListener('click', () => {
+		inputAddTrek.click()
+	})
 
-  inputAddCardPhoto.setAttribute('accept', '.jpg, .jpeg, .png, .webp');
-  inputAddCardPhoto.addEventListener('change', changeHandler);
+	inputAddCardPhoto.setAttribute('accept', '.jpg, .jpeg, .png, .webp')
+	inputAddCardPhoto.addEventListener('change', changeHandler)
 
-  inputAddDescPhoto.setAttribute('accept', '.jpg, .jpeg, .png, .webp');
-  inputAddDescPhoto.setAttribute('multiple', true);
-  inputAddDescPhoto.addEventListener('change', changeHandlerImages);
+	inputAddDescPhoto.setAttribute('accept', '.jpg, .jpeg, .png, .webp')
+	inputAddDescPhoto.setAttribute('multiple', true)
+	inputAddDescPhoto.addEventListener('change', changeHandlerImages)
 
-  inputAddTrek.setAttribute('accept', '.fit, .gpx, .tcx');
-  inputAddTrek.addEventListener('change', changeHandlerTrek);
-  const boxCardPhoto = document.querySelector('#input__card-photo-images');
-  const boxCardPhotoDesc = document.querySelector('#input__desc-photo-images');
-  const svgCard = document.querySelector('#input__card-photo-img');
-  const svgDesc = document.querySelector('#input__desc-photo-img');
+	inputAddTrek.setAttribute('accept', '.fit, .gpx, .tcx')
+	inputAddTrek.addEventListener('change', changeHandlerTrek)
+	const boxCardPhoto = document.querySelector('#input__card-photo-images')
+	const boxCardPhotoDesc = document.querySelector('#input__desc-photo-images')
+	const svgCard = document.querySelector('#input__card-photo-img')
+	const svgDesc = document.querySelector('#input__desc-photo-img')
 
-  function changeHandler(event) {
-    if (!event.target.files.length) {
-      return;
-    }
+	function changeHandler(event) {
+		if (!event.target.files.length) {
+			return
+		}
 
-    let file = event.target.files[0];
-    boxCardPhoto.innerHTML = '';
+		let file = event.target.files[0]
+		boxCardPhoto.innerHTML = ''
 
-    //проверка на картинку
-    if (!file.type.match('image')) {
-      return;
-    }
-    //обработчик события, так как reader.readAsDataURL асинхронная функция
-    const reader = new FileReader();
-    reader.onload = async (ev) => {
-      const src = ev.target.result;
-      //проверка в какой блок добавляется фотография
-      //далее соответствующее уменьшение размеров фотографии
-      //запись файлов в объект data
-      const cardPhoto = await reduceImage(src, 250);
+		//проверка на картинку
+		if (!file.type.match('image')) {
+			return
+		}
+		//обработчик события, так как reader.readAsDataURL асинхронная функция
+		const reader = new FileReader()
+		reader.onload = async ev => {
+			const src = ev.target.result
+			//проверка в какой блок добавляется фотография
+			//далее соответствующее уменьшение размеров фотографии
+			//запись файлов в объект data
+			const cardPhoto = await reduceImage(src, 450)
 
-      let sizeFile = Math.trunc(file.size / 8000);
-      boxCardPhoto.insertAdjacentHTML(
-        'beforeend',
+			let sizeFile = Math.trunc(file.size / 8000)
+			boxCardPhoto.insertAdjacentHTML(
+				'beforeend',
 
-        `<div class="box__preview">
+				`<div class="box__preview">
           <div class="box__preview-remove" data-name="${file.name}">&times</div>
             <img src="${cardPhoto}" id="photo-desc-img"/>
             <div class="box__preview-name">
@@ -74,48 +74,46 @@ export default function loadimg(data) {
               <span>${sizeFile}kB</span>
             </div>
           </div>`
-      );
+			)
 
-      svgCard.classList.add('notEmpty');
-    };
-    reader.readAsDataURL(file);
-  }
+			svgCard.classList.add('notEmpty')
+		}
+		reader.readAsDataURL(file)
+	}
 
-  boxCardPhoto.addEventListener('click', (event) => {
-    if (!event.target.dataset.name) return;
-    const name = event.target.dataset.name;
-    svgCard.classList.remove('notEmpty');
-    const block = boxCardPhoto
-      .querySelector(`[data-name="${name}"]`)
-      .closest('.box__preview');
-    block.classList.add('removing');
-    setInterval(() => {
-      block.remove();
-    }, 300);
-  });
+	boxCardPhoto.addEventListener('click', event => {
+		if (!event.target.dataset.name) return
+		const name = event.target.dataset.name
+		svgCard.classList.remove('notEmpty')
+		const block = boxCardPhoto.querySelector(`[data-name="${name}"]`).closest('.box__preview')
+		block.classList.add('removing')
+		setInterval(() => {
+			block.remove()
+		}, 300)
+	})
 
-  let arrPhoto = [];
-  function changeHandlerImages(event) {
-    if (!event.target.files.length) {
-      return;
-    }
-    const files = Array.from(event.target.files);
+	let arrPhoto = []
+	function changeHandlerImages(event) {
+		if (!event.target.files.length) {
+			return
+		}
+		const files = Array.from(event.target.files)
 
-    files.forEach((file) => {
-      if (!file.type.match('image')) {
-        return;
-      }
+		files.forEach(file => {
+			if (!file.type.match('image')) {
+				return
+			}
 
-      const reader = new FileReader();
-      reader.onload = async (ev) => {
-        const src = ev.target.result;
+			const reader = new FileReader()
+			reader.onload = async ev => {
+				const src = ev.target.result
 
-        const srcSmall = await reduceImage(src, 700);
-        arrPhoto.push({ filename: file.name, srcSmall });
-        let sizeFile = Math.trunc(file.size / 8000);
-        boxCardPhotoDesc.insertAdjacentHTML(
-          'beforeend',
-          `
+				const srcSmall = await reduceImage(src, 700)
+				arrPhoto.push({ filename: file.name, srcSmall })
+				let sizeFile = Math.trunc(file.size / 8000)
+				boxCardPhotoDesc.insertAdjacentHTML(
+					'beforeend',
+					`
           <div class="box__preview">
           <div class="box__preview-remove" data-name="${file.name}">&times</div>
             <img src="${srcSmall}" id="photo-desc-img" />
@@ -125,54 +123,52 @@ export default function loadimg(data) {
             </div>
           </div>
           `
-        );
+				)
 
-        svgDesc.classList.add('notEmpty');
-      };
-      reader.readAsDataURL(file);
-    });
-  }
+				svgDesc.classList.add('notEmpty')
+			}
+			reader.readAsDataURL(file)
+		})
+	}
 
-  boxCardPhotoDesc.addEventListener('click', (event) => {
-    if (!event.target.dataset.name) return;
-    const name = event.target.dataset.name;
-    arrPhoto = arrPhoto.filter((file) => file.filename !== name);
+	boxCardPhotoDesc.addEventListener('click', event => {
+		if (!event.target.dataset.name) return
+		const name = event.target.dataset.name
+		arrPhoto = arrPhoto.filter(file => file.filename !== name)
 
-    const block = boxCardPhotoDesc
-      .querySelector(`[data-name="${name}"]`)
-      .closest('.box__preview');
-    block.classList.add('removing');
-    setInterval(() => {
-      block.remove();
-    }, 300);
+		const block = boxCardPhotoDesc.querySelector(`[data-name="${name}"]`).closest('.box__preview')
+		block.classList.add('removing')
+		setInterval(() => {
+			block.remove()
+		}, 300)
 
-    if (arrPhoto.length === 0) {
-      svgDesc.classList.remove('notEmpty');
-    }
-  });
+		if (arrPhoto.length === 0) {
+			svgDesc.classList.remove('notEmpty')
+		}
+	})
 
-  function changeHandlerTrek(event) {
-    if (!event.target.files.length) {
-      return;
-    }
+	function changeHandlerTrek(event) {
+		if (!event.target.files.length) {
+			return
+		}
 
-    const svg = document.getElementById(this.id + '-img');
+		const svg = document.getElementById(this.id + '-img')
 
-    const newSpan = document.getElementById('trek-status-text');
-    if (newSpan) {
-      newSpan.remove();
-    }
-    const file = event.target.files[0];
-    const fileName = file.name;
-    let dataFileTrek = new FormData();
-    dataFileTrek.append('filedata', file);
-    data.fileTrek = dataFileTrek;
-    data.fileTrekName = fileName;
+		const newSpan = document.getElementById('trek-status-text')
+		if (newSpan) {
+			newSpan.remove()
+		}
+		const file = event.target.files[0]
+		const fileName = file.name
+		let dataFileTrek = new FormData()
+		dataFileTrek.append('filedata', file)
+		data.fileTrek = dataFileTrek
+		data.fileTrekName = fileName
 
-    svg.insertAdjacentHTML(
-      'beforebegin',
-      `<span class="box__status-text" id="trek-status-text">${fileName}</span>`
-    );
-    svg.classList.add('notEmpty');
-  }
+		svg.insertAdjacentHTML(
+			'beforebegin',
+			`<span class="box__status-text" id="trek-status-text">${fileName}</span>`
+		)
+		svg.classList.add('notEmpty')
+	}
 }
